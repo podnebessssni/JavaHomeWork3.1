cddf3f5ac5c1de4f3efa7acc937b85ea7076e14a
Oleg Georgiyevski <oleqg@yahoo.com>
2020-06-09 22:03:02 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 25000;
+        int miles = cost / 20;
+        System.out.println("За покупку этого билет, вы получите " + miles + " бонусных миль");
+    }
+}
